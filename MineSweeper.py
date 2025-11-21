910a89e3b7e7633953c0744fdb29d397e39992fb
Qincai695 <qincai_ya@163.com>
2025-11-21 21:44:52 +0800

    Yee~

--- MineSweeper.py
+++ MineSweeper.py
@@ -12,13 +12,19 @@ root.title("MineSweeper")
 game = Toplevel(root)
 game.title("MineSweeper")
 # 菜单界面
+# 标题
 title_label = Label(root, text="扫雷", font=("宋体", 30), compound=CENTER, width=13)
 title_label.grid(column=0, row=0, columnspan=2)
+# 大小设置
 item_label = Label(root, text="尺寸: ")
 item_label.grid(column=0, row=1, sticky=E)
 size_spin = Spinbox(root, from_=2, to=25)
 size_spin.grid(column=1, row=1, sticky=S)
+# 开始按钮
 start_button = Button(root, text="开始游戏")
 start_button.grid(column=0, row=2, columnspan=2)
+# 退出按钮
+exit_button = Button(root, text="退出游戏", command=root.destroy)
+exit_button.grid(column=0, row=3, columnspan=2)
 # 进入消息循环
 root.mainloop()
